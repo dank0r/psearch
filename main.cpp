90eb057c568af2c38ac78eebc9f1c339c147f06a
dank0r <dan296dan@gmail.com>
2019-11-11 14:55:18 +0300

    change file reading method to mmap

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 #include <thread>
 #include <filesystem>
 #include <fstream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 
 namespace fs = std::filesystem;
 using std::cout;
@@ -19,43 +26,78 @@ using std::thread;
 using std::getline;
 using std::ifstream;
 
-void add_strings(string &file_path, vector<tuple<string, int, string> > &strings) {
-	ifstream in(file_path.c_str());
-	if(!in) {
-		cerr << "Cannot open file " << file_path << endl;
+struct line {
+	char *begin;
+	char *end;
+	int file_id;
+};
+
+struct file {
+	int id;
+	string path;
+	char *buf;
+	int fd;
+	size_t fs;
+};
+
+void add_lines(file &f, vector<line> &lines) {
+	f.fd = open(f.path.c_str(), O_RDONLY);
+	if(f.fd == -1) {
+		cerr << "Cannot open file " << f.path << endl;
 	}
-	string str;
-	int i = 0;
-	while(getline(in, str)) {
-		i++;
-		if(str.size() > 0) {
-			strings.push_back(make_tuple(str, i, file_path));
+	struct stat fs;
+	if(fstat(f.fd, &fs) == -1) {
+		cerr << "Cannot open file " << f.path << endl;
+		close(f.fd);
+	}
+	f.fs = static_cast<size_t>(fs.st_size);
+
+	//place for madvise and posix_fadvise
+	
+	f.buf = static_cast<char *>(mmap(NULL, f.fs, PROT_READ, MAP_SHARED, f.fd, 0));
+	if(f.buf == MAP_FAILED) {
+		cerr << "Cannot memory map file " << f.path << endl;
+		close(f.fd);
+	}
+	char *buf_end = f.buf + fs.st_size;
+	char *begin = f.buf;
+	char *end = NULL;
+	while((end = static_cast<char *>(memchr(begin, '\n', static_cast<size_t>(buf_end - begin)))) != NULL) {
+		line l {begin, end, f.id};
+		lines.push_back(l);
+		if(end != buf_end) {
+			begin = end + 1;
+		} else {
+			break;
 		}
 	}
-}
-
-bool search(string &str, string &substr) {
-	return str.find(substr) != string::npos;
+	close(f.fd);
 }
 
 int main() {
 	int n_threads = 4;
 	string dir_path = "/home/dank0r/Desktop/text";
 	string substr = "the";
-	vector<tuple<string, int, string> > strings;
+	vector<line> lines;
 	vector<thread> threads(n_threads);
-	
+	vector<file> files;
 	for(auto &p : fs::recursive_directory_iterator(dir_path)) {
 		if(fs::is_regular_file(p)) {
 			string file_path = p.path().string();
-			add_strings(file_path, strings);
+			int id = files.size();
+			file f {id, file_path};
+			files.push_back(f);
+			add_lines(f, lines);
 		}
 	}
-	for(auto &occ : strings) {
-		if(search(get<0>(occ), substr)) {
-			cout << "Found occurrence in file " << get<2>(occ) << ":" << endl;
-			cout << get<1>(occ) << ": " << get<0>(occ) << endl;
+	for(auto &occ : lines) {
+		for(auto it = occ.begin; it != occ.end; it++) {
+			cout << *it;
 		}
+		cout << endl;
+	}
+	for(auto &f : files) {
+		munmap(f.buf, f.fs);
 	}
 	
 }
